a0d90a7c0f83c7e3eb67e67b81884335a1f874f3
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2015-08-30 00:37:56 -0400

    hightrustapps

---
+++ HighTrustAppWeb/CachingProvider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Web;
+
+namespace HighTrustAppWeb
+{
+    public interface ICachingProvider
+    {
+        T GetFromCache<T>(string key, Func<T> cacheMissCallback);
+    }
+    public class CachingProvider : ICachingProvider
+    {
+        protected MemoryCache _cache = MemoryCache.Default;
+        protected static readonly object padLock = new object();
+        private const int DEFAULT_CACHE_LIFETIME_MINUTES = 60;
+
+        private void AddItem(string key, object value)
+        {
+            lock (padLock)
+            {
+                CacheItem item = new CacheItem(key, value);
+                CacheItemPolicy policy = new CacheItemPolicy();
+                policy.AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(DEFAULT_CACHE_LIFETIME_MINUTES);
+                policy.RemovedCallback = new CacheEntryRemovedCallback((args) =>
+                {
+                    if (args.CacheItem.Value is IDisposable)
+                    {
+                        ((IDisposable)args.CacheItem.Value).Dispose();
+                    }
+                });
+                _cache.Set(item, policy);
+            }
+        }
+
+        private object GetItem(string key)
+        {
+            lock (padLock)
+            {
+                var result = _cache[key];
+                return result;
+            }
+        }
+
+        public T GetFromCache<T>(string key, Func<T> cacheMissCallback)
+        {
+            var objectFromCache = GetItem(key);
+            T objectToReturn = default(T);
+            if (objectFromCache == null)
+            {
+                objectToReturn = cacheMissCallback();
+                if (objectToReturn != null)
+                {
+                    AddItem(key, objectToReturn);
+                }
+            }
+            else
+            {
+                if (objectFromCache is T)
+                {
+                    objectToReturn = (T)objectFromCache;
+                }
+            }
+            return objectToReturn;
+        }
+    }
+}
---
+++ HighTrustAppWeb/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HighTrustAppWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly ICachingProvider _cache = new CachingProvider();
+        [SharePointContextFilter]
+        public ActionResult Index()
+        {
+            
+
+            var spContext = SharePointContextProvider.Current.GetSharePointContext(HttpContext);
+
+            using (var clientContext = spContext.CreateUserClientContextForSPHost())
+            {
+                if (clientContext != null)
+                {
+                    ViewBag.SiteName = _cache.GetFromCache<string>("SiteTitle", () =>
+                    {
+                        return GetSiteTitle(clientContext);
+                    });
+                }
+            }
+
+            return View();
+        }
+
+        private string GetSiteTitle(ClientContext clientContext)
+        {
+            Web spWeb = clientContext.Web;
+            clientContext.Load(spWeb, web => web.Title);
+            clientContext.ExecuteQuery();
+            return spWeb.Title;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
